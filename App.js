921d7d5a08df415499ef9b8f7de80e6f90169407
sagarkhadka <khadkasagar109@gmail.com>
2024-04-02 00:50:48 +0545

    image select

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
 
 import ImageViewer from './components/ImageViewer'
 import { Button } from './components/Button'
@@ -7,13 +9,35 @@ import { Button } from './components/Button'
 const placeholderImage = require('./assets/images/image.jpg')
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    })
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri)
+    } else {
+      alert('You did not select any image.')
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={placeholderImage} />
+        <ImageViewer
+          placeholderImageSource={placeholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button label='Choose a photo' />
+        <Button
+          theme='primary'
+          label='Choose a photo'
+          onPress={pickImageAsync}
+        />
         <Button label='User this photo' />
       </View>
       <StatusBar style='auto' />
--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -1,7 +1,12 @@
 import { StyleSheet, Image } from 'react-native'
 
-export default function ImageViewer({ placeholderImageSource }) {
-  return <Image source={placeholderImageSource} style={styles.image} />
+export default function ImageViewer({ placeholderImageSource, selectedImage }) {
+  console.log(selectedImage)
+  const imageSource = selectedImage
+    ? { uri: selectedImage }
+    : placeholderImageSource
+
+  return <Image source={imageSource} style={styles.image} />
 }
 
 const styles = StyleSheet.create({
