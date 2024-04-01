3babddfbd74b4953c1e915cd623a074a0bb0247a
Sagar Khadka <khadkasagar109@gmail.com>
2024-04-01 23:22:20 +0545

    image container and button comp

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
+
+import ImageViewer from './components/ImageViewer'
+import { Button } from './components/Button'
+
+const placeholderImage = require('./assets/images/image.jpg')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={placeholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label='Choose a photo' />
+        <Button label='User this photo' />
+      </View>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#25292e',
+    alignItems: 'center'
   },
-});
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58
+  },
+
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center'
+  }
+})
